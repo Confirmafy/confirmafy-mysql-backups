b4cddcaef5ae47c6c2a895ec190c1895a90f6d8f
Daniel Macario <danielmacario5@gmail.com>
2026-02-05 13:43:17 -0500

    modify command to produce a single file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,10 @@
 set -euo pipefail
 
 # shellcheck disable=SC2086
-mydumper --host "$MYSQL_HOST" --user $MYSQL_USER --password $MYSQL_PASSWORD --port $MYSQL_PORT --database $MYSQL_DATABASE -C -c --clear -o backup
+# --threads 0 means "use the number of CPU cores"
+# -v 3 means "verbose level 3" - includes info logs
+# --stream writes a single stream to stdout (TRADITIONAL = stream then delete each file); we capture it to one file
+mydumper --host "$MYSQL_HOST" --user $MYSQL_USER --password $MYSQL_PASSWORD --port $MYSQL_PORT --database $MYSQL_DATABASE -c --clear --threads 0 -v 3 --stream -o backup > backup.stream.tmp && mv backup.stream.tmp backup/backup.stream
 rclone config touch
 cat <<EOF > ~/.config/rclone/rclone.conf
 [remote]
